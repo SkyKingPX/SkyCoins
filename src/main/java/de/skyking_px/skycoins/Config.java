a05fa1dc5995ded1ea26e6963e9f2fbc41bb0489
BuildTools <antony.blem@icloud.com>
2024-07-01 20:36:09 +0200

    Initial commit

---
+++ src/main/java/de/skyking_px/skycoins/Config.java
@@ -0,0 +1,36 @@
+package de.skyking_px.skycoins;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
+// Demonstrates how to use Forge's config APIs
+@Mod.EventBusSubscriber(modid = SkyCoins.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config
+{
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    private static final ForgeConfigSpec.BooleanValue DISABLE_COIN_RECIPE = BUILDER
+            .comment("Would you like the Coin to be crafted or to be unique?")
+            .define("disableCoin1Recipe", false);
+
+    static final ForgeConfigSpec SPEC = BUILDER.build();
+
+    public static boolean disableCoinRecipe;
+
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event)
+    {
+        disableCoinRecipe = DISABLE_COIN_RECIPE.get();
+    }
+}
